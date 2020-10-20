d08d0c33a70f5ade369b6f424779c4cbcd5797ee
東澔 <donghaozhao_a2@163.com>
2020-10-20 09:35:06 +0800

    modify auth rules

--- server/auth/index.ts
+++ server/auth/index.ts
@@ -1,16 +1,5 @@
 const filterList = [
-  '/api/login',
-  '/api/get-module',
-  '/api/auth/check',
-  '/api/img/list',
-  '/api/jqfy',
-  '/api/yysb',
-  '/api/yyhc',
-  '/api/wzsb',
-  '/api/txsb',
-  '/api/ljwm',
-  '/static',
-  '/api/get-website',
+  '/'
 ];
 
 export function WhiteList(url: string): boolean {
